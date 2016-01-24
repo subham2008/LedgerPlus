da0dd34542113abcddd1ce7aaa8f2778991ff57b
Subham <subham2008@gmail.com>
2016-01-25 00:37:57 +0530

    v 1.2.1

--- app/src/main/java/com/example/subham/ledgerplus/BackupActivity.java
+++ app/src/main/java/com/example/subham/ledgerplus/BackupActivity.java
@@ -1,9 +1,14 @@
 package com.example.subham.ledgerplus;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class BackupActivity extends ActionBarActivity {
@@ -12,28 +17,22 @@ public class BackupActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backup);
+
+        //Get the IMEI number to generate unique ID
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        final String imeiId = telephonyManager.getDeviceId();
+
+        Button b = (Button) findViewById(R.id.buttonBackupContinue);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BackupActivity.this,imeiId,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_backup, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
